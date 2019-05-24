116aec817dbccee2602b987b1bf85d6ff02d11f3
whiteRocket <aljaz.podgornik@t-2.net>
2019-05-24 17:28:52 +0200

    500.000 stvari ki se malo premikajo (v resnici je samo eno telo)

---
+++ Assets/koda/Spinn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinn : MonoBehaviour
+{
+    public Transform trans;
+    float kot = 0f;
+    public float hitrost = (2 * Mathf.PI) / 10;
+    public float radij = 5;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        kot += hitrost * Time.deltaTime;
+        trans.position = new Vector3(Mathf.Cos(kot) * radij,
+        Mathf.Sin(kot) * radij, trans.position.z);
+        
+    }
+}
---
+++ Assets/koda/ustvari.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ustvari : MonoBehaviour
+{
+    public GameObject zvezda;
+    GameObject stars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stars = new GameObject();
+        
+        for (int i = 1; i < 500001; i++) {
+            //var zacZvezda = zvezda;
+
+            //zacZvezda.transform.position = new Vector3(Random.Range(-5000f, 5000f), Random.Range(-5000f, 5000f), 0);
+
+            GameObject sin = Instantiate(zvezda, new Vector3(Random.Range(-5000f, 5000f), Random.Range(-5000f, 5000f), 0), Quaternion.identity);
+
+            sin.transform.parent = stars.transform;
+
+            //zacZvezda.GetComponent<Spinn>().radij = i *0.001f;
+            //zacZvezda.GetComponent<Spinn>().hitrost = Random.Range(0.5f, 10f);
+            
+        }
+        Instantiate(stars, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    float kot = 0f;
+    public float hitrost = (2 * Mathf.PI) / 10;
+    public float radij = 5;
+    void Update()
+
+    {
+        kot += hitrost * Time.deltaTime;
+        stars.transform.position = new Vector3(Mathf.Cos(kot) * radij,
+        Mathf.Sin(kot) * radij, stars.transform.position.z);
+
+    }
+}
